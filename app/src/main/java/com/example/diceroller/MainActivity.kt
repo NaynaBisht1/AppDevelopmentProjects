232d8b3a7adbfcd43b1e3108014dc8bc40f36f8f
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-08-13 02:37:55 +0530

    Implementing Random No. Logic

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import java.util.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,20 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+//        rollButton.text = "Let's Roll"
 
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
 
+    }
+
+    private fun rollDice() {
+        val resultText : TextView = findViewById(R.id.result_text)
+        val randomInt = Random().nextInt(6) + 1
+//        resultText.text = "Dice Rolled !"
+        resultText.text = randomInt.toString()
     }
 }
